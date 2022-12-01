df3b95441343c513d261057ef978b0dd2af1be8d
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-11-30 23:24:08 -0300

    Ideia inicial de resolução

---
+++ Trabalho_1/IA.py
@@ -0,0 +1,22 @@
+''' Descrição do problema: 
+Você tem 2 jarros sem nenhuma marcação, com as respectivas capacidades: 1º 5 litros e o 2º 3 litros
+. O propósito do problema é que ocorra uma situação final de que o jarro de maior capacidade 
+fique com exatamente 1 litro.
+. Você pode executar as seguintes ações:
+      • Enxer totalmente um jarro com a água do poço;
+      • Esvaziar totalmente um jarro jogando a água na grama;
+      • Passar a água de um jarro para o outro.
+      • Os jarros começam vazios.
+Um ponto a observar é que a movimentação exige o cuidado de não ultrapassar a capacidade dos respectivos jarros.
+
+buscas: backtracking, em largura e em profundidade
+'''
+
+class busca_backtracking:
+      ''' Classe que é responsavel pela busca em backtracking '''
+
+class busca_largura:
+      ''' Classe que é responsavel pela busca em largura '''
+
+class busca_profundidade:
+      ''' Classe que é responsavel pela busca em profundidade '''
