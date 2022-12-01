adc3c09d132f057e759a89ac3715b5b18af532b2
MathewsJosh <mathews.gomes@engenharia.ufjf.br>
2022-12-01 20:46:52 -0300

    Adição  de "encapsulamento" busca backtracking

--- Trabalho_1/buscas.py
+++ Trabalho_1/buscas.py
@@ -29,21 +29,22 @@ class busca:
             if(len(regras) != 6):
                   raise AttributeError('O número de regras deve ser 6')
             self.regras = regras
-            self.action = self.regras_to_action()
+            self.action = self.__regras_to_action()
             self.objetivo_jarro_a = objetivo_jarro_a
             self.objetivo_jarro_b = objetivo_jarro_b
+            self.graph = None
       
-      def encher_a(self, node_pai: graph.node) -> graph.node:
+      def __encher_a(self, node_pai: graph.node) -> graph.node:
             '''Encher o jarro A'''
             node = graph.node(5, node_pai.capacidade_b)
             return node
 
-      def encher_b(self, node_pai: graph.node) -> graph.node:
+      def __encher_b(self, node_pai: graph.node) -> graph.node:
             '''Encher o jarro B'''
             node = graph.node(node_pai.capacidade_a, 3)
             return node
 
-      def passar_a_para_b(self, node_pai: graph.node) -> graph.node:
+      def __passar_a_para_b(self, node_pai: graph.node) -> graph.node:
             '''Passar a água do jarro A para o jarro B'''
             if(node_pai.capacidade_a + node_pai.capacidade_b > 3):
                   node = graph.node(node_pai.capacidade_a - (3 - node_pai.capacidade_b), 3)
@@ -51,7 +52,7 @@ class busca:
                   node = graph.node(0, node_pai.capacidade_a + node_pai.capacidade_b)
             return node
       
-      def passar_b_para_a(self, node_pai: graph.node) -> graph.node:
+      def __passar_b_para_a(self, node_pai: graph.node) -> graph.node:
             '''Passar a água do jarro B para o jarro A'''
             if(node_pai.capacidade_a + node_pai.capacidade_b > 5):
                   node = graph.node(5, node_pai.capacidade_b - (5 - node_pai.capacidade_a))
@@ -59,51 +60,88 @@ class busca:
                   node = graph.node(node_pai.capacidade_a + node_pai.capacidade_b, 0)
             return node
 
-      def esvaziar_a(self, node_pai: graph.node) -> graph.node:
+      def __esvaziar_a(self, node_pai: graph.node) -> graph.node:
             '''Esvaziar o jarro A'''
             node = graph.node(0, node_pai.capacidade_b)
             return node
 
-      def esvasiar_b(self, node_pai: graph.node) -> graph.node:
+      def __esvasiar_b(self, node_pai: graph.node) -> graph.node:
             '''Esvaziar o jarro B'''
             node = graph.node(node_pai.capacidade_a, 0)
             return node
 
-      def regras_to_action(self) -> list:
+      def __regras_to_action(self) -> list:
             retorno = []
             for i in range(len(self.regras)):
                   if(self.regras[i] == ['E','A']):
-                        retorno.append(self.encher_a)
+                        retorno.append(self.__encher_a)
                   elif(self.regras[i] == ['E','B']):
-                        retorno.append(self.encher_b)
+                        retorno.append(self.__encher_b)
                   elif(self.regras[i] == ['A','B']):
-                        retorno.append(self.passar_a_para_b)
+                        retorno.append(self.__passar_a_para_b)
                   elif(self.regras[i] == ['B','A']):
-                        retorno.append(self.passar_b_para_a)
+                        retorno.append(self.__passar_b_para_a)
                   elif(self.regras[i] == ['V','A']):
-                        retorno.append(self.esvaziar_a)
+                        retorno.append(self.__esvaziar_a)
                   elif(self.regras[i] == ['V','B']):
-                        retorno.append(self.esvasiar_b)
+                        retorno.append(self.__esvasiar_b)
                   else:
                         raise AttributeError('Regra inválida')
             return retorno
 
-      def verificacao_regras(self, node_pai: graph.node, graph: graph.graph) -> bool:
+      def __verificacao_regras(self, node_pai: graph.node) -> bool:
             '''Verifica se a regra aplicada gera um estado valido para ser colocado no grafo'''
             for i in range(len(self.regras)):
-                  node = self.regras[i](node_pai)
-                  if(not graph.verifica_se_exixte_no_caminho(node, node_pai)):
+                  node = self.action[i](node_pai)
+                  if(not self.graph.verifica_se_exixte_no_caminho(node, node_pai)):
                         return True
             return False
 
+      def __add_node_to_graph(self, node_pai: graph.node) -> graph.node:
+            if(self.__verificacao_regras(node_pai)):
+                  for i in range(len(self.regras)):
+                        node = self.action[i](node_pai)
+                        if(not self.graph.verifica_se_exixte_no_caminho(node, node_pai)):
+                              return node
+
+      def __verifica_visinhanca(self, node: graph.node) -> bool:
+            if(node.pai == None):
+                  return True
+            else:
+                  for auxiliar in range(len(node.pai.filhos)):
+                        if(node.pai.filhos[auxiliar].capacidade_b == node.capacidade_b and node.pai.filhos[auxiliar].capacidade_a == node.capacidade_a):
+                              return False
+                  return True
+                  
       def busca_em_profundidade(self, capacidade_inicial_a:int = 0, capacidade_inicial_b:int = 0, to_string:bool = False) -> graph.graph:
             '''Busca em profundidade'''
+            if(self.graph != None):
+                  self.graph = None
             self.graph = graph.graph("profundidade",capacidade_inicial_a, capacidade_inicial_b)
-      
+
+
       def busca_em_largura(self, capacidade_inicial_a:int = 0, capacidade_inicial_b:int = 0, to_string:bool = False) -> graph.graph:
             '''Busca em largura'''
+            if(self.graph != None):
+                  self.graph = None
             self.graph = graph.graph("largura",capacidade_inicial_a, capacidade_inicial_b)
 
       def busca_em_backtracking(self, capacidade_inicial_a:int = 0, capacidade_inicial_b:int = 0, to_string:bool = False) -> graph.graph:
             '''Busca em backtracking'''
+            if(self.graph != None):
+                  self.graph = None
             self.graph = graph.graph("backtracking",capacidade_inicial_a, capacidade_inicial_b)
+
+            lista_abertos = [self.graph.raiz]
+            lista_fechados = []
+
+            while(lista_abertos[-1].capacidade_a != self.objetivo_jarro_a):
+                  if(self.__verificacao_regras(lista_abertos[-1])):
+                        node = self.__add_node_to_graph(lista_abertos[-1])
+                        if(not self.graph.verifica_se_exixte_no_caminho(node, lista_abertos[-1]) and self.__verifica_visinhanca(node)):
+                              lista_abertos[-1].add_filho(node)
+                              lista_abertos.append(node)
+                  else:
+                        lista_fechados.append(lista_abertos.pop(-1))
+
+            return self.graph
