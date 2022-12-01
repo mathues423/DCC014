6d7b2095546fe67eb0d8b98a36fdcc2ee10ab379
Matheus dos Reis Casarim <37851549+mathues423@users.noreply.github.com>
2022-11-30 22:26:20 -0300

    adição do corpo do projeto

---
+++ Trabalho_1/graph.py
@@ -0,0 +1,8 @@
+class node:
+      def __init__(self, id: str):
+
+
+class graph:
+      def __init__(self, tipo: str):
+            self.tipo = tipo
+            self.raiz = node(0,0)
---
+++ Trabalho_1/main.py
@@ -0,0 +1,3 @@
+import graph
+if __name__ == '__main__':
+      
