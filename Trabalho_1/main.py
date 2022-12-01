df690e5d5a8a5d21f9fb323324503efed5069279
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-11-30 23:24:26 -0300

    Adição do "main"

--- Trabalho_1/main.py
+++ Trabalho_1/main.py
@@ -1,3 +1,5 @@
 import graph
 if __name__ == '__main__':
-      
+      graph_backtracking = graph.graph("backtracking")
+      graph_largura = graph.graph("largura")
+      graph_profundidade = graph.graph("profundidade")
