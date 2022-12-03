14287cbf8d93cd08df43abb2674b69f08120d4ff
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-12-03 16:34:12 -0300

    main com as buscas

--- Trabalho_1/main.py
+++ Trabalho_1/main.py
@@ -4,10 +4,11 @@ from buscas import busca
 #[['V','B'],['B','A'],['E','B'],['V','A'],['E','A'],['A','B']]
 if __name__ == '__main__':
       regras = [['V','B'],['B','A'],['E','B'],['V','A'],['E','A'],['A','B']]
-      #random.shuffle(regras)
+      random.shuffle(regras)
       main = busca(regras,1)
-      main.busca_em_backtracking(to_string=True)
-      main.busca_em_largura(to_string=True)      
+      main.busca_em_backtracking(verbose=True)
+      main.busca_em_largura(verbose=True)
+      main.busca_em_profundidade(verbose=True)      
       #main.
       # graph_backtracking = buscas
       # graph_largura = 
