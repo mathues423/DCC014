48518a39cb70d2dfef65924549ff51adeba79f53
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-12-02 23:35:43 -0300

    testes no main

--- Trabalho_1/main.py
+++ Trabalho_1/main.py
@@ -3,11 +3,11 @@ import random
 from buscas import busca
 #[['V','B'],['B','A'],['E','B'],['V','A'],['E','A'],['A','B']]
 if __name__ == '__main__':
-      regras = [['E','A'],['V','B'],['B','A'],['V','A'],['A','B'],['E','B']]
-      random.shuffle(regras)
+      regras = [['V','B'],['B','A'],['E','B'],['V','A'],['E','A'],['A','B']]
+      #random.shuffle(regras)
       main = busca(regras,1)
       main.busca_em_backtracking(to_string=True)
-            
+      main.busca_em_largura(to_string=True)      
       #main.
       # graph_backtracking = buscas
       # graph_largura = 
